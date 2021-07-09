c7743289acd75613cb6b6437193cecc49b62b01d
Matt Weiss <weiss.matt@outlook.com>
2021-07-09 17:10:56 -0600

    Adds board and initial styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import board from './assets/board.png'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="craps">
+      <div className="game-container">
+        <div className="game-info">info here</div>
+        <img src={board} className="play-surface" alt=""/>
+      </div>
     </div>
   );
 }
