904985387f41c0255f871bd7993b15dacede4ceb
Matt Weiss <weiss.matt@outlook.com>
2021-07-09 18:45:26 -0600

    Adds component

---
+++ src/components/dice.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class Die extends Component {
+  render() {
+    return (
+        <div className="die">
+
+        </div>
+    )
+  }
+
+}
+
+export default Die
